e3add5a33718f90db015f2c57b0869a18f1224da
Mohamed Soliman <mohamed.soliman@aalto.fi>
2016-12-09 17:22:18 +0200

    fetching params from request

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -5,9 +5,13 @@ module Api::V1
     # GET /v1/search
     def search
     	query =  params['query']
-    	puts "in index"
+    	platforms = params['platforms'] || "youtube,periscope"
+    	num = params['num'] || 1
+    	puts query
+    	puts platforms
+    	puts num
     	response = HTTParty.get('https://www.googleapis.com/youtube/v3/search?key='+ENV["google_api_key"]+'&maxResults=5&part=id,snippet&q=test&eventType=live&type=video')
-    	puts $twitter_client.search("Renan preside o Senado neste momento: o triunfo do cinismo e a derrota do Estado de Direito filter:periscope").to_json
+    	#puts $twitter_client.search("Renan preside o Senado neste momento: o triunfo do cinismo e a derrota do Estado de Direito filter:periscope").to_json
     	#puts response.body, response.code, response.message, response.headers.inspect
     	render json: {"search_query":query}
     end
