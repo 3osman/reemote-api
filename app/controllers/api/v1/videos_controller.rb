14cf160591067f05c6d76c1cef0e352f5acad0fd
Mohamed Soliman <mohamed.soliman@aalto.fi>
2016-12-09 20:47:37 +0200

    returning browser friendly urls

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -1,4 +1,4 @@
-# app/controllers/api/v1/users_controller.rb
+# app/controllers/api/v1/videos_controller.rb
 module Api::V1
   class VideosController < ApiController
 
@@ -9,23 +9,27 @@ module Api::V1
     	num = params['num'] || 1
     	you_vids = get_youtube(query,num)
     	#get_periscope(query,num)
-    	#puts response.body, response.code, response.message, response.headers.inspect
     	render json: {"response":you_vids}
     end
 
     def get_youtube(query,num)
-    	#maxResults = "&maxResults=5"
-    	maxResults = ""
+    	maxResults = "&maxResults=50"
     	response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?key="+ENV["google_api_key"]+"#{maxResults}"+"&part=id&q=#{query}&eventType=live&type=video&videoEmbeddable=true")
-    	#ids = response['items'].map{ |it| it['id']['videoId']}.join(',')
-    	#video_players = HTTParty.get("https://www.googleapis.com/youtube/v3/videos?key="+ENV["google_api_key"]+"&part=player&id=#{ids}")
-    	#vids = Array.new
     	video_players = response['items'].map{ |it| "//www.youtube.com/embed/#{it['id']['videoId']}"}.join(',')
-    	return video_players
+    	video_urls = response['items'].map {|it| "https://www.youtube.com/watch?v=#{it['id']['videoId']}"}.join(',')
+    	return video_urls
     end
+    
     def get_periscope(query,num)
     	$twitter_client.search("#{query} filter:periscope").to_json
     end
 
   end
 end
+
+
+
+
+#ids = response['items'].map{ |it| it['id']['videoId']}.join(',')
+    	#video_players = HTTParty.get("https://www.googleapis.com/youtube/v3/videos?key="+ENV["google_api_key"]+"&part=player&id=#{ids}")
+    	#vids = Array.new
